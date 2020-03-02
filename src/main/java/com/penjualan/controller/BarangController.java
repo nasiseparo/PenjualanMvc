a08bbfaa7df38823c93c4639520e74cc6ebd9928
nasiseparo <dedyrizaldi02@gmail.com>
2020-03-02 17:16:30 +0700

    update frontend

--- src/main/java/com/penjualan/controller/BarangController.java
+++ src/main/java/com/penjualan/controller/BarangController.java
@@ -1,5 +1,34 @@
 package com.penjualan.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
 public class BarangController {
+	
+	@RequestMapping("/add")
+	public String Add(Model model) {
+
+		return "add_transaction";
+	}
+	
+	@RequestMapping("/list")
+	public String List(Model model) {
+		
+		return "list_transaction";
+	}
+	
+	@RequestMapping("/update")
+	public String Update(Model model) {
+		
+		return "update_transaction";
+	}
+	@RequestMapping("/home")
+	public String Home(Model model) {
+		
+		return "home";
+	}
 
 }
