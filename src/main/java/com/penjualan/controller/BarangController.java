62b5ecf92192cfef2aa05ebc67e313f0aadb1b24
Freizello <hendriktio.freizello@gmail.com>
2020-03-03 09:51:43 +0700

    Database change and minor bugs on barang controller

--- src/main/java/com/penjualan/controller/BarangController.java
+++ src/main/java/com/penjualan/controller/BarangController.java
@@ -1,37 +1,9 @@
 package com.penjualan.controller;
 
-<<<<<<< HEAD
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
-@Controller
-public class BarangController {
-	
-	@RequestMapping("/add")
-	public String Add(Model model) {
-
-		return "add_transaction";
-	}
-	
-	@RequestMapping("/list")
-	public String List(Model model) {
-		
-		return "list_transaction";
-	}
-	
-	@RequestMapping("/update")
-	public String Update(Model model) {
-		
-		return "update_transaction";
-	}
-	@RequestMapping("/home")
-	public String Home(Model model) {
-		
-		return "home";
-	}
-=======
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -73,8 +45,6 @@ public class BarangController {
 		model.addAttribute("dto", dto);
 		return "add_barang";
 	}
-	
->>>>>>> hendrik
 
 	@RequestMapping("/barang/edit/{kodebarang}")
 	public String detail(@PathVariable("kodebarang") String kodeBarang, Model model) {
