e5122dc43c55f79720cfaea2df4fcc43071cc5f0
0167 <0167@DESKTOP-1STLEUU>
2020-03-03 10:16:26 +0700

    Update Provinsi Function

--- src/main/java/com/penjualan/controller/BarangController.java
+++ src/main/java/com/penjualan/controller/BarangController.java
@@ -15,7 +15,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import com.penjualan.dto.MstCustomerDto;
+import com.penjualan.dto.MstProvinsiDto;
 import com.penjualan.service.MstCustomerSvc;
+import com.penjualan.service.MstProvinsiSvc;
 
 @Controller
 public class BarangController {
@@ -23,11 +25,12 @@ public class BarangController {
 	
 	@Autowired
 	private MstCustomerSvc CustomerSvc;
+	@Autowired
+	private MstProvinsiSvc ProvinsiSvc;
 	
 	
 	
-	
-//	---------------------Crud Customer ---------------------------------------------------------
+//	---------------------Start of Crud Customer ---------------------------------------------------------
 	
 		@RequestMapping("/listcustomer")
 		public String listCustomer(Model model) {
@@ -49,7 +52,7 @@ public class BarangController {
 		public String save(@Valid @ModelAttribute("customerAdd") MstCustomerDto dto,
 				BindingResult result) {
 			if (result.hasErrors()) {
-				return "CustomerAdd";
+				return "customerAdd";
 			} else {
 				CustomerSvc.save(dto);
 				return "redirect:/listcustomer";
@@ -95,4 +98,66 @@ public class BarangController {
 		
 //		---------------------End of Crud Customer ---------------------------------------------------------
 		
+		
+//		---------------------Start of Crud Provinsi ---------------------------------------------------------
+		
+		@RequestMapping("/listprovinsi")
+		public String listProvinsi(Model model) {
+			List<MstProvinsiDto> dtos = ProvinsiSvc.listAll();
+			model.addAttribute("provinsis", dtos);
+			return "provinsiList";
+
+	}
+		
+		@RequestMapping("/addprovinsi")
+		public String addProvinsi(Model model) {
+			MstProvinsiDto dto = new MstProvinsiDto();
+			model.addAttribute("provinsiAdd", dto);
+			return "provinsiAdd";
+		}
+		
+		
+		@RequestMapping("/saveprovinsi")
+		public String saveProvisi(@Valid @ModelAttribute("provinsiAdd") MstProvinsiDto dto,
+				BindingResult result) {
+			if (result.hasErrors()) {
+				return "provinsiAdd";
+			} else {
+				ProvinsiSvc.save(dto);
+				return "redirect:/listprovinsi";
+			}
+		}
+		
+		
+		@RequestMapping(value = "/editprovinsi/{kode}")
+		public String detailProvinsi(@PathVariable("kode") String kode, Model model) {
+			MstProvinsiDto provinsiDto = ProvinsiSvc.getById(kode);
+			model.addAttribute("kodeProvinsi", provinsiDto.getKodeProvinsi());
+			model.addAttribute("namaProvinsi", provinsiDto.getNamaProvinsi());
+			return "provinsiEdit";
+		}
+
+		
+		@RequestMapping(value = "/updateprovinsi", method = RequestMethod.POST)
+		public String updateProvinsi(@Valid @ModelAttribute("provinsi") MstProvinsiDto dto,
+				BindingResult result) {
+			if (result.hasErrors()) {
+				return "editprovinsi";
+			} else {
+				ProvinsiSvc.update(dto);
+				return "redirect:/listprovinsi";
+			}
+		}
+		
+		
+		@RequestMapping(value = "/delete/{kode}")
+		public String deleteProvinsi(@PathVariable("kode") String kode, Model model) {
+			if(kode!=null){
+				ProvinsiSvc.delete(kode);
+				return "redirect:/listprovinsi";
+			}else{
+				return "redirect:/listprovinsi";
+			}
+		}
+
 }
